758a4482f5cf4d2b29f8aa686cdeeb41a06203c4
Diego Ramos <diegorramos84@gmail.com>
2022-09-02 11:13:11 +0100

    create a logger.js to handle info/error

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -17,10 +18,10 @@ const Blog = mongoose.model('Blog', blogSchema)
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
   .then(result => {
-    console.log('connected to MondoDB')
+    logger.info('connected to MondoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB', error.message)
+    logger.error('error connecting to MongoDB', error.message)
   })
 
 app.use(cors())
@@ -46,5 +47,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
