c0e2bdd1833f4543bcaec969b613592d3b4a5b03
Diego Ramos <diegorramos84@gmail.com>
2022-09-05 14:53:22 +0100

    add comments to index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const http = require('http')
+const http = require('http') // require node.js built-in module to transfer data over http
 const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const server = http.createServer(app)
+const server = http.createServer(app) // assign createServer http module to the variable server and call the app
 
-server.listen(config.PORT, () => {
+server.listen(config.PORT, () => { // Bind and listen for connections on the given host and port
   logger.info(`Server running on port ${config.PORT}`)
 })
