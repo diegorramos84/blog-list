4990510c8efcc0ed0765da5ba6b3982401ff7eee
Diego Ramos <diegorramos84@gmail.com>
2022-09-08 16:14:02 +0100

    add favorite blog test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,26 @@ const totalLikes = (blogs) => blogs.reduce(
   (sum, blog) =>
   { return blog.likes + sum }, 0)
 
+const favoriteBlog = (blogs) => {
+  // assign first blog.likes to max
+  // compare with the next blog.likes iteration and keep the higher value
+  const favorite = blogs.reduce((fav, blog) => {
+    if(fav.likes > blog.likes) {
+      return fav
+    } else {
+      return fav = blog
+    }
+  })
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
