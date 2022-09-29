487d4c7f04c728b54554ec97e6161c41e019c225
Diego Ramos <diegorramos84@gmail.com>
2022-09-29 14:42:05 +0100

    add json and id test

--- tests/blogList.test.js
+++ tests/blogList.test.js
@@ -17,8 +17,24 @@ beforeEach(async () => {
 
 test('blog application returns correct amout of blog posts', async() => {
   const response = await api.get('/api/blogs')
-
   expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property of the blog post is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const content = response.body
+  console.log(content)
+
+  const idChecker = content.map(r => r.id)
+  console.log(idChecker)
+  expect(idChecker[0]).toBeDefined()
 })
 
 afterAll(() => {
