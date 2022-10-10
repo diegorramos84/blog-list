cfd84462f7396a1118714ba9180d5188aadcae8a
Diego Ramos <diegorramos84@gmail.com>
2022-10-10 18:48:44 +0100

    add get all users functionality

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
@@ -17,6 +18,11 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
 })
 
 module.exports = usersRouter
