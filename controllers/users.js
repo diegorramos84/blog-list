e067630334dea6abfc037c156c7cde392a902bbc
Diego Ramos <diegorramos84@gmail.com>
2022-10-18 16:07:47 +0100

    add password and username min length validation

--- controllers/users.js
+++ controllers/users.js
@@ -5,19 +5,29 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+  console.log(username.length)
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  // validate username and password > 3 chars long
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error: 'username & password need to be at least 3 chars long'
+    })
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.status(201).json(savedUser)
+    response.status(201).json(savedUser)
+  }
+  // username must be unique
+  // respond code and error
 })
 
 usersRouter.get('/', async (request, response) => {
