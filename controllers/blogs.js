f992ae692d11552d9067d587d1dce47832294ca5
Diego Ramos <diegorramos84@gmail.com>
2022-10-20 15:24:36 +0100

    add func wher only authorized user can create blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
-  if(authorization && authorization.toLowerCase().startsWith('bearer')) {
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
     return authorization.substring(7)
   }
   return null
@@ -20,8 +20,15 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
